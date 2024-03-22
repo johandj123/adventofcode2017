6e6f1dec4160b2f44c4485357eaa703f35025d8d
Johan de Jong <johan.de.jong@sogeti.com>
2024-03-22 11:24:23 +0100

    Day 13.

---
+++ src/Day13.java
@@ -0,0 +1,51 @@
+import lib.InputUtil;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Day13 {
+    public static void main(String[] args) throws IOException {
+        Map<Integer, Integer> map = InputUtil.readAsLines("input13.txt").stream()
+                .map(s -> s.split(": "))
+                .collect(Collectors.toMap(sp -> Integer.parseInt(sp[0]), sp -> Integer.parseInt(sp[1])));
+        first(map);
+        second(map);
+    }
+
+    private static void first(Map<Integer, Integer> map) {
+        int severity = map.entrySet().stream()
+                .filter(entry -> (entry.getKey() % period(entry.getValue())) == 0)
+                .mapToInt(entry -> entry.getKey() * entry.getValue())
+                .sum();
+        System.out.println(severity);
+    }
+
+    private static void second(Map<Integer, Integer> map) {
+        for (int i = 0; ; i++) {
+            if (!caught(map, i)) {
+                System.out.println(i);
+                break;
+            }
+        }
+    }
+
+    private static boolean caught(Map<Integer, Integer> map,int startTime) {
+        return map.entrySet().stream()
+                .anyMatch(entry -> ((entry.getKey() + startTime) % period(entry.getValue())) == 0);
+    }
+
+    private static int period(int range) {
+        return 2 * (range - 1);
+    }
+
+    private static int position(int range,int time) {
+        int period = period(range);
+        int x = time % period;
+        if (x < range) {
+            return x;
+        } else {
+            return period - x;
+        }
+    }
+}
