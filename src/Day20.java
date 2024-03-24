b7ef34330f596a94eedb779725d0c30cdd730c95
Johan de Jong <johan.de.jong@sogeti.com>
2024-03-24 13:05:38 +0100

    Day 20 part 2.

--- src/Day20.java
+++ src/Day20.java
@@ -10,10 +10,15 @@ public class Day20 {
         List<Particle> particles = InputUtil.readAsLines("input20.txt").stream()
                 .map(Particle::new)
                 .collect(Collectors.toList());
-        int smallest = Integer.MAX_VALUE;
+        first(particles);
+        second(particles);
+    }
+
+    private static void first(List<Particle> particles) {
+        long smallest = Long.MAX_VALUE;
         int index = -1;
         for (int i = 0; i < particles.size(); i++) {
-            int value = particles.get(i).maxAccelMagnatude();
+            long value = particles.get(i).maxAccelMagnatude();
             if (value < smallest) {
                 smallest = value;
                 index = i;
@@ -22,10 +27,36 @@ public class Day20 {
         System.out.println(index);
     }
 
+    private static void second(List<Particle> particles) {
+        for (int count = 0; count < 10000; count++) {
+            particles.forEach(Particle::step);
+
+            boolean change;
+            do {
+                change = false;
+                for (int i = 0; i < particles.size(); i++) {
+                    Particle a = particles.get(i);
+                    for (int j = particles.size() - 1; j > i; j--) {
+                        Particle b = particles.get(j);
+                        if (a.collision(b)) {
+                            particles.remove(j);
+                            change = true;
+                        }
+                    }
+                    if (change) {
+                        particles.remove(i);
+                        break;
+                    }
+                }
+            } while (change);
+        }
+        System.out.println(particles.size());
+    }
+
     static class Particle {
-        int[] x = new int[3];
-        int[] v = new int[3];
-        int[] a = new int[3];
+        long[] x = new long[3];
+        long[] v = new long[3];
+        long[] a = new long[3];
 
         // Direct formula for the position:
         // x=at^2+v0t+x0
@@ -33,21 +64,34 @@ public class Day20 {
         Particle(String s) {
             String[] sp = s.split("[^\\d-]+");
             for (int i = 0; i < 3; i++) {
-                x[i] = Integer.parseInt(sp[i + 1]);
+                x[i] = Long.parseLong(sp[i + 1]);
             }
             for (int i = 0; i < 3; i++) {
-                v[i] = Integer.parseInt(sp[i + 4]);
+                v[i] = Long.parseLong(sp[i + 4]);
             }
             for (int i = 0; i < 3; i++) {
-                a[i] = Integer.parseInt(sp[i + 7]);
+                a[i] = Long.parseLong(sp[i + 7]);
             }
         }
 
-        int maxAccelMagnatude() {
+        long maxAccelMagnatude() {
             return Arrays.stream(a)
                     .map(Math::abs)
                     .max()
                     .orElseThrow();
+        }
+
+        void step() {
+            for (int i = 0; i < 3; i++) {
+                v[i] += a[i];
+            }
+            for (int i = 0; i < 3; i++) {
+                x[i] += v[i];
+            }
+        }
+
+        boolean collision(Particle o) {
+            return Arrays.equals(x, o.x);
         }
     }
 }
